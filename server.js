c90d4b658491b9761b1e3352be372b505b007675
avenu128 <44643177+avenu128@users.noreply.github.com>
2019-11-20 11:24:02 -0500

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.get('/api', (req, res) => {
   const baseURL = 'https://data.princegeorgescountymd.gov/resource/9hyf-46qb.json';
   fetch(baseURL)
     .then((r) => r.json())
-    .then((course) => {course.filter(c => c.dept_id === "INST")})
-    .then((course) => course.map(c => c.course_id + ": " + c.name))
     .then((result) => {
     console.log(result);
       res.send({ data: result });
